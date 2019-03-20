e6f7a4d31a19fc90742c43a06f0ad44d5e30afbc
Adrimi <adriano100417@gmail.com>
2019-03-20 13:50:31 +0100

    Initial commit

---
+++ Homework2/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Homework2
+//
+//  Created by Adrimi on 20/03/2019.
+//  Copyright © 2019 Adrimi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
